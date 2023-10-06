6366874d6c2b546a0eab0fab2b9d1d0cfb4ceafd
Kate test <d.swetlov2017@gmail.com>
2023-10-06 17:42:23 +0300

    update

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,4 +10,23 @@ public class StatisticsService {
         }
         return currentMax;
     }
+
+    public long findMin(long[] incomes) {
+        long currentMin = incomes[0];
+        for (long income : incomes) {
+            if (currentMin > income) {
+                currentMin = income;
+            }
+        }
+        return currentMin;
+    }
+
+    public int calculateSum(long[] incomes) {
+        long currentMin = incomes[0];
+        long sum = 0;
+        for (long income : incomes) {
+            sum += income;
+        }
+        return (int) sum;
+    }
 }
